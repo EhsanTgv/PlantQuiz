1d5736506351e07b9e72495fb4f9763b4e078b90
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-12 18:58:28 +0330

    Creating Plant Data class

---
+++ app/src/main/java/com/taghavi/plantquiz/Plant.kt
@@ -0,0 +1,12 @@
+package com.taghavi.plantquiz
+
+class Plant(
+    genus: String,
+    species: String,
+    cultivar: String,
+    common: String,
+    pictureName: String,
+    description: String,
+    difficulty: Int,
+    id: Int = 0
+)
