5e40560998fd6eb60fefe93108b8ff1556e7230c
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-13 20:28:51 +0330

    Main Activity Implement Intent to camera

--- app/src/main/java/com/taghavi/plantquiz/MainActivity.kt
+++ app/src/main/java/com/taghavi/plantquiz/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.taghavi.plantquiz
 
+import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val openCameraButtonRequestId = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        openCameraButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, openCameraButtonRequestId)
+        }
     }
 
     fun buttonOneIsClicked(view: View) {
