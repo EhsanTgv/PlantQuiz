70adb77cf8491de6db25c3a068d359fbda7c88e5
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-15 23:41:04 +0330

    Connecting to server from ParsePlantUtility

--- app/src/main/java/com/taghavi/plantquiz/models/ParsePlantUtility.kt
+++ app/src/main/java/com/taghavi/plantquiz/models/ParsePlantUtility.kt
@@ -2,6 +2,9 @@ package com.taghavi.plantquiz.models
 
 class ParsePlantUtility {
     fun parsePlantObjectsFromJsonData(search: String): List<Plant> {
+        val allPlantsObject :ArrayList<Plant> = ArrayList()
+        val downloadingObject = DownloadingObject().downloadJSONDataFrom("http://plantplaces.com/perl/mobile/flashcard.pl")
 
+        return  allPlantsObject
     }
 }
