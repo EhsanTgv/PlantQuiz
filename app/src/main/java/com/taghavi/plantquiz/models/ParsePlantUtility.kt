ab802a8119274c39faa4e6108e6aade89d0ec97b
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-15 23:45:49 +0330

    ParsePlantUtility Initial data as JsonArray named "values"

--- app/src/main/java/com/taghavi/plantquiz/models/ParsePlantUtility.kt
+++ app/src/main/java/com/taghavi/plantquiz/models/ParsePlantUtility.kt
@@ -1,10 +1,16 @@
 package com.taghavi.plantquiz.models
 
+import org.json.JSONObject
+
 class ParsePlantUtility {
     fun parsePlantObjectsFromJsonData(search: String): List<Plant> {
-        val allPlantsObject :ArrayList<Plant> = ArrayList()
-        val downloadingObject = DownloadingObject().downloadJSONDataFrom("http://plantplaces.com/perl/mobile/flashcard.pl")
+        val allPlantsObject: ArrayList<Plant> = ArrayList()
+        val downloadingObject = DownloadingObject()
+        val topLevelPlantJsonData =
+            DownloadingObject().downloadJSONDataFrom("http://plantplaces.com/perl/mobile/flashcard.pl")
+        val topLevelJsonObject = JSONObject(topLevelPlantJsonData)
+        val plantObjectArray = topLevelJsonObject.getJSONArray("values")
 
-        return  allPlantsObject
+        return allPlantsObject
     }
 }
